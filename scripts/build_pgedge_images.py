b029304ba410ba121333c24ee097befd90ab6b3f
Muhammad Aqeel <muhammad.aqeel@pgedge.com>
2026-07-30 15:54:10 +0500

    Remove staging channel from build script

--- scripts/build_pgedge_images.py
+++ scripts/build_pgedge_images.py
@@ -166,7 +166,6 @@ all_images: list[PgEdgeImage] = [
         epoch=1,
         is_latest_for_pg_major=True,
         is_latest_for_spock_major=True,
-        package_release_channel="staging",
     ),
     # pg17 images
     *make_all_flavor_images(
@@ -175,7 +174,6 @@ all_images: list[PgEdgeImage] = [
         epoch=1,
         is_latest_for_pg_major=True,
         is_latest_for_spock_major=True,
-        package_release_channel="staging",
     ),
     # pg18 images
     *make_all_flavor_images(
@@ -184,7 +182,6 @@ all_images: list[PgEdgeImage] = [
         epoch=1,
         is_latest_for_pg_major=True,
         is_latest_for_spock_major=True,
-        package_release_channel="staging",
     ),
     # pg16 spock60 images
     *make_all_flavor_images(
@@ -193,7 +190,6 @@ all_images: list[PgEdgeImage] = [
         epoch=2,
         is_latest_for_pg_major=True,
         is_latest_for_spock_major=True,
-        package_release_channel="staging",
     ),
     # pg17 spock60 images
     *make_all_flavor_images(
@@ -202,7 +198,6 @@ all_images: list[PgEdgeImage] = [
         epoch=2,
         is_latest_for_pg_major=True,
         is_latest_for_spock_major=True,
-        package_release_channel="staging",
     ),
     # pg18 spock60 images
     *make_all_flavor_images(
@@ -211,7 +206,6 @@ all_images: list[PgEdgeImage] = [
         epoch=2,
         is_latest_for_pg_major=True,
         is_latest_for_spock_major=True,
-        package_release_channel="staging",
     ),
 ]
 
